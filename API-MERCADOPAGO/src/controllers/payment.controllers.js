872604046f6715086f22568bbcb387f50f9f1c7a
nicoortiz1 <77462897+nicoortiz1@users.noreply.github.com>
2024-07-10 12:36:42 -0300

    cambios en el body

--- API-MERCADOPAGO/src/controllers/payment.controllers.js
+++ API-MERCADOPAGO/src/controllers/payment.controllers.js
@@ -4,34 +4,79 @@ import { MercadoPagoConfig, Preference } from 'mercadopago';
 export const createOrder = async (req, res) => {
     try {
 
-        const  client  =  new  MercadoPagoConfig ( {  accessToken : 'TEST-7068168195646950-100721-dc40ac3495008e6a85f15b2b20a24374-229906939' ,  options : {  timeout : 5000 ,  idempotencyKey : 'abc'  }  } ) ;
+        const  client  =  new  MercadoPagoConfig ( {  accessToken : 'TEST-6702637773184768-070510-7e66b2bcf0948334db02a21411a23fa4-229906939' , integrator_id: 'dev_24c65fb163bf11ea96500242ac130004', options : {  timeout : 5000 ,  idempotencyKey : 'abc'  }  } ) ;
 
         // Crear instancia de Preference
         const preference = new Preference (client);
 
         // Definir los detalles de la preferencia
         const body = {
-            items: [{
-                title: "Libro",
-                quantity: 1,
-                unit_price: 100,
-                currency_id: "ARS",
-            }],
-            back_urls: {
-                success: "https://www.mercadopago.com.ar/developers/",
-                failure: "https://www.mercadopago.com.ar/developers/",
-                pending: "https://www.mercadopago.com.ar/developers/"
+            "items": [
+                {
+                    "id": "item-ID-1234",
+                    "title": "Mi producto",
+                    "currency_id": "ARS",
+                    "picture_url": "https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
+                    "description": "Descripción del Item",
+                    "category_id": "art",
+                    "quantity": 1,
+                    "unit_price": 75.76
+                }
+            ],
+            "payer": {
+                "name": "Juan",
+                "surname": "Lopez",
+                "email": "user@email.com",
+                "phone": {
+                    "area_code": "11",
+                    "number": "4444-4444"
+                },
+                "identification": {
+                    "type": "DNI",
+                    "number": "12345678"
+                },
+                "address": {
+                    "street_name": "Street",
+                    "street_number": 123,
+                    "zip_code": "5700"
+                }
             },
-            auto_return: "approved",
+            "back_urls": {
+                "success": "https://www.success.com",
+                "failure": "https://www.failure.com",
+                "pending": "https://www.pending.com"
+            },
+            "auto_return": "approved",
+            "payment_methods": {
+                "excluded_payment_methods": [
+                    {
+                        "id": "master"
+                    }
+                ],
+                "excluded_payment_types": [
+                    {
+                        "id": "ticket"
+                    }
+                ],
+                "installments": 12
+            },
+            "notification_url": "https://www.your-site.com/ipn",
+            "statement_descriptor": "MINEGOCIO",
+            "external_reference": "Reference_1234",
+            "expires": true,
+            //"expiration_date_from": "2016-02-01T12:00:00.000-04:00",
+            //"expiration_date_to": "2016-02-28T12:00:00.000-04:00"
         };
         // Paso 5: Crear objeto de opciones de solicitud - Opcional 
-        const  requestOptions  =  { 
+        /*const  requestOptions  =  { 
             idempotencyKey : '<IDEMPOTENCY_KEY>' , 
-        } ;
+        } ;*/
 
     
-        preference.create({ body, requestOptions }).then(console.log).catch(console.log);
+        preference.create({ body /*requestOptions*/}).then(console.log).catch(console.log);
 
+        
+        
     } catch (error) {
         console.error('Error al crear la preferencia:', error);
         res.status(500).json({
